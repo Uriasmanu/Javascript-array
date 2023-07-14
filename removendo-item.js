b5a632fc5dfa4332abdb671870deb279ec2bb04d
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-07-14 16:24:25 -0300

    Create removendo-item.js

---
+++ removendo-item.js
@@ -0,0 +1,10 @@
+const notas = [10, 3, 6.8, 9, 7];
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas)
+const media = (notas[0] + notas[1] + notas[2] + notas[3]);
+
+console.log(`A media é $(media)`)
