b358dfc1dfc85fb5d282b67c5cac17eeb9117ae6
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-07-15 00:08:51 -0300

    .

---
+++ atualizandoLista.js
@@ -0,0 +1,12 @@
+const nomes = [
+    "João",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Leo"
+];
+//   remove a partir do 1, 2 itens e adiciona Rodrigo
+nomes.splice(1, 2, "Rodrigo");
+
+console.log(nomes);
