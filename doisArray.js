fff22bcf9442240e6eda385f03c846feb42da62d
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-07-16 20:05:07 -0300

    .

---
+++ doisArray.js
@@ -0,0 +1,8 @@
+const nomes = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const nomeseMedias = [nomes, medias];
+
+console.log(nomeseMedias);
+
+console.log(`A aluna da posição 1 da lista de alunos é: ${nomeseMedias[0][1]}.`);
---
+++ juntandoArray.js
@@ -0,0 +1,9 @@
+const salaJs = ["João", "Juliana", "Ana", "Caio"];
+const salaPyton = ["Antonio", "Geovana", "Rafaela"];
+
+const novaSala = salaPyton.concat(salaJs);
+
+console.log(salaJs);
+console.log(salaPyton);
+
+console.log(novaSala);
